9a892468ea6d915f3ca0b31052b07569683a6cc2
TheGodSlayer <alexandrerider@hotmail.com>
2022-06-23 12:47:47 -0300

    new adjustment with the methods, utilizing For

--- src/main/java/chess/Board.java
+++ src/main/java/chess/Board.java
@@ -6,6 +6,16 @@ public class Board {
     private final ArrayList<ArrayList<Piece>> allRanks;
     public Board (){
         this.allRanks = new ArrayList<>();
+
+        for (int i = 0; i < 8; i++) {
+            allRanks.add(new ArrayList<>());
+        }
+        for (int i = 0; i < 8 ; i++) {
+            for (int j = 0; j < 8; j++) {
+                allRanks.get(i).add(Piece.noPiece());
+            }
+        }
+
     }
     public int getNumberOfAllPieces(){
         int count = 0;
@@ -18,42 +28,39 @@ public class Board {
         return count;
     }
     public void initialize(){
-        for (int i = 0; i < 8; i++) {
-            allRanks.add(new ArrayList<>());
-        }
-        allRanks.get(0).add(Piece.createWhiteRook());
-        allRanks.get(0).add(Piece.createWhiteKnight());
-        allRanks.get(0).add(Piece.createWhiteBishop());
-        allRanks.get(0).add(Piece.createWhiteQueen());
-        allRanks.get(0).add(Piece.createWhiteKing());
-        allRanks.get(0).add(Piece.createWhiteBishop());
-        allRanks.get(0).add(Piece.createWhiteKnight());
-        allRanks.get(0).add(Piece.createWhiteRook());
+        allRanks.get(0).set(0,Piece.createWhiteRook());
+        allRanks.get(0).set(1,Piece.createWhiteKnight());
+        allRanks.get(0).set(2,Piece.createWhiteBishop());
+        allRanks.get(0).set(3,Piece.createWhiteQueen());
+        allRanks.get(0).set(4,Piece.createWhiteKing());
+        allRanks.get(0).set(5,Piece.createWhiteBishop());
+        allRanks.get(0).set(6,Piece.createWhiteKnight());
+        allRanks.get(0).set(7,Piece.createWhiteRook());
 
-        for (int rank = 0; rank < 8 ; rank++) {
-            allRanks.get(1).add(Piece.createWhitePawn());
-            allRanks.get(2).add(Piece.noPiece());
-            allRanks.get(3).add(Piece.noPiece());
-            allRanks.get(4).add(Piece.noPiece());
-            allRanks.get(5).add(Piece.noPiece());
-            allRanks.get(6).add(Piece.createBlackPawn());
+        for (int file = 0; file < 8 ; file++) {
+            allRanks.get(1).set(file,Piece.createWhitePawn());
+            allRanks.get(2).set(file,Piece.noPiece());
+            allRanks.get(3).set(file,Piece.noPiece());
+            allRanks.get(4).set(file,Piece.noPiece());
+            allRanks.get(5).set(file,Piece.noPiece());
+            allRanks.get(6).set(file,Piece.createBlackPawn());
         }
 
-        allRanks.get(7).add(Piece.createBlackRook());
-        allRanks.get(7).add(Piece.createBlackKnight());
-        allRanks.get(7).add(Piece.createBlackBishop());
-        allRanks.get(7).add(Piece.createBlackQueen());
-        allRanks.get(7).add(Piece.createBlackKing());
-        allRanks.get(7).add(Piece.createBlackBishop());
-        allRanks.get(7).add(Piece.createBlackKnight());
-        allRanks.get(7).add(Piece.createBlackRook());
+        allRanks.get(7).set(0,Piece.createBlackRook());
+        allRanks.get(7).set(1,Piece.createBlackKnight());
+        allRanks.get(7).set(2,Piece.createBlackBishop());
+        allRanks.get(7).set(3,Piece.createBlackQueen());
+        allRanks.get(7).set(4,Piece.createBlackKing());
+        allRanks.get(7).set(5,Piece.createBlackBishop());
+        allRanks.get(7).set(6,Piece.createBlackKnight());
+        allRanks.get(7).set(7,Piece.createBlackRook());
     }
     public String ranksRepresentation(){
         StringBuilder buffer = new StringBuilder();
         String jump = "";
-        for (int i = 7; i > 0 ; i--) {
-            buffer.append(allRanks.get(i).get(i).getRepresentation() + jump);
-            for (int j = 0; j < allRanks.size(); j++) {
+        for (int i = 7; i >= 0 ; i--) {
+            buffer.append(jump + allRanks.get(i).get(i).getRepresentation());
+            for (int j = 1; j < allRanks.size(); j++) {
                 buffer.append(allRanks.get(i).get(j).getRepresentation());
                 if (allRanks.get(i) == allRanks.get(7)){
                     jump = StringUtil.appendNewLine("");
@@ -63,65 +70,20 @@ public class Board {
         return buffer.toString();
     }
 
-    public void emptyBoard(){
-        for (int rank = 0; rank < 8; rank++) {
-            for (int i = 0; i < allRanks.size(); i++) {
-                allRanks.get(i).set(rank, Piece.noPiece());
-            }
-        }
-    }
-    public String anotherPrintBoardSolution(){
+    public String printBoard(){
         StringBuilder buffer = new StringBuilder();
         buffer.append(StringUtil.appendNewLine(ranksRepresentation()));
         return buffer.toString();
     }
-    public String printBoardWithoutPieces(){
-        StringBuilder buffer = new StringBuilder();
 
-        return buffer.toString();
-    }
-
-    public int pieceCount(char representation) {
+    public int pieceCountTurboVersion(char representation){
         int count = 0;
-        for (Piece knight: allRanks.get(0)){
-            if (knight.getRepresentation() == representation){
-                count++;
-            }
-        }
-        for (Piece piece: allRanks.get(1)){
-            if (piece.getRepresentation() == representation){
-                count++;
-            }
-        }
-        for (Piece piece: allRanks.get(2)){
-            if (piece.getRepresentation() == representation){
-                count++;
-            }
-        }
-        for (Piece piece: allRanks.get(3)){
-            if (piece.getRepresentation() == representation){
-                count++;
-            }
-        }
-        for (Piece piece: allRanks.get(4)){
-            if (piece.getRepresentation() == representation){
-                count++;
-            }
-        }
-        for (Piece piece: allRanks.get(5)){
-            if (piece.getRepresentation() == representation){
-                count++;
-            }
-        }
-        for (Piece piece: allRanks.get(6)){
-            if (piece.getRepresentation() == representation){
-                count++;
-            }
-        }
-        for (Piece piece: allRanks.get(7)){
-            if (piece.getRepresentation() == representation){
-                count++;
-            }
+        for (int i = 0; i < 8; i++) {
+            allRanks.get(i).get(i);
+            for (Piece piece: allRanks.get(i))
+                if (piece.getRepresentation() == representation){
+                    count++;
+                }
         }
         return count;
     }
@@ -131,90 +93,30 @@ public class Board {
 
         file -= 'a';
 
-        if (rank == '1'){
-            return allRanks.get(0).get(file);
-        }
-        else if (rank == '2'){
-            return allRanks.get(1).get(file);
-        }
-        else if (rank == '3'){
-            return allRanks.get(2).get(file);
-        }
-        else if (rank == '4'){
-            return allRanks.get(3).get(file);
-        }
-        else if (rank == '5') {
-            return allRanks.get(4).get(file);
-        }
-        else if (rank == '6'){
-            return allRanks.get(5).get(file);
-        }
-        else if (rank == '7'){
-            return allRanks.get(6).get(file);
-        }
-        else
-            return allRanks.get(7).get(file);
+        rank -= '1';
+
+        return allRanks.get(rank).get(file);
+
     }
 
-    public void placePiece(String position,Piece create){
+    public void placePieceRemastered(String position, Piece allocate){
         char file = position.charAt(0);
         char rank = position.charAt(1);
 
         file -= 'a';
 
-        if (rank == '1'){
-             allRanks.get(0).set(file, create);
-        }
-        else if (rank == '2') {
-            allRanks.get(1).set(file, create);
-        }
-        else if (rank == '3') {
-            allRanks.get(2).set(file, create);
-        }
-        else if (rank == '4') {
-            allRanks.get(3).set(file, create);
-        }
-        else if (rank == '5') {
-            allRanks.get(4).set(file, create);
-        }
-        else if (rank == '6') {
-            allRanks.get(5).set(file, create);
-        }
-        else if (rank == '7') {
-            allRanks.get(6).set(file, create);
-        }
-        allRanks.get(7).set(file, create);
+        rank -= ' ';
+
+        allRanks.get(rank).set(file, allocate);
     }
 
-    public double pawnStrength(){
-        return 0.5;
+    public double basicStrength(){
+        double someStrength = 0;
+        double queenStrength = 9 + someStrength;
+        double rookStrength = 5 + someStrength;
+        double bishopStrength = 3 + someStrength;
+        double knightStrength = 2.5 + someStrength;
+        double pawnStrength = 1 + someStrength;
+        return someStrength;
     }
-    public int rookStrength(){
-        return 5;
-    }
-    public double knightStrength(){
-        return 2.5;
-    }
-    public int bishopStrength(){
-        return 3;
-    }
-    public int queenStrength(){
-        return 9;
-    }
-//    public int kingStrength(){
-//
-//    }
-    public double getPawnStrength(){
-        return pawnStrength();
-    }
-    public int getRookStrength(){
-        return rookStrength();
-    }
-    public int getBishopStrength(){
-        return bishopStrength();
-    }
-
-//    public int PieceStrength(int strength){
-//
-//    }
 }
--- src/test/java/chess/TestBoard.java
+++ src/test/java/chess/TestBoard.java
@@ -25,89 +25,69 @@ public class TestBoard {
                         blankRank + blankRank + blankRank + blankRank +
                         StringUtil.appendNewLine("pppppppp") +
                         StringUtil.appendNewLine("rnbqkbnr"),
-                board.anotherPrintBoardSolution());
-        System.out.println(board.anotherPrintBoardSolution());
+                board.printBoard());
+        System.out.println(board.printBoard());
         System.out.println();
 
         Piece.resetCounter();
     }
-//    @Test
-//    public void testNumberOfPiecesByRepresentation(){
-//        board.initialize();
-//        assertEquals(2, board.pieceCount('r'));
-//        assertEquals(2, board.pieceCount('n'));
-//        assertEquals(2 ,board.pieceCount('b'));
-//        assertEquals(1, board.pieceCount('q'));
-//        assertEquals(1, board.pieceCount('k'));
-//
-//        assertEquals(8, board.pieceCount('p'));
-//
-//        assertEquals(8, board.pieceCount('P'));
-//
-//        assertEquals(2, board.pieceCount('R'));
-//        assertEquals(2, board.pieceCount('N'));
-//        assertEquals(2, board.pieceCount('B'));
-//        assertEquals(1, board.pieceCount('Q'));
-//        assertEquals(1, board.pieceCount('K'));
-//        Piece.resetCounter();
-//    }
-//    @Test
-//    public void testRetrievePieceByLocation(){
-//        board.initialize();
-//        System.out.println();
-//        assertEquals('R', board.getPiece("a8").getRepresentation());
-//        assertEquals('N', board.getPiece("b8").getRepresentation());
-//        assertEquals('B', board.getPiece("c8").getRepresentation());
-//        assertEquals('n', board.getPiece("b1").getRepresentation());
-//
-//    }
-//    @Test
-//    public void testEmptyBoard(){
-//        board.initialize();
-//        board.emptyBoard();
-//        assertEquals(
-//                StringUtil.appendNewLine("........") +
-//                        StringUtil.appendNewLine("........") +
-//                        StringUtil.appendNewLine("........") +
-//                        StringUtil.appendNewLine("........") +
-//                        StringUtil.appendNewLine("........") +
-//                        StringUtil.appendNewLine("........") +
-//                        StringUtil.appendNewLine("........") +
-//                        StringUtil.appendNewLine("........"),
-//                board.printBoardWithoutPieces());
-//    }
-//    @Test
-//    public void testPieceByLocation(){
-//        board.initialize();
-//        board.emptyBoard();
-//        board.placePiece("b6",Piece.createBlackKing());
-//        assertEquals('K', board.getPiece("b6").getRepresentation());
-//        board.placePiece("b7",Piece.createBlackKing());
-//        assertEquals('K', board.getPiece("b7").getRepresentation());
-//    }
-//    @Test
-//    public void testPieceStrength(){
-//        board.initialize();
-//        board.emptyBoard();
-//
-//        board.placePiece("b8",Piece.createBlackKing());
-//        assertEquals('K', board.getPiece("b8").getRepresentation());
-//
-//        board.placePiece("c8",Piece.createBlackRook());
-//        assertEquals('R', board.getPiece("c8").getRepresentation());
-//        assertEquals(5, board.getRookStrength());
-//
-//        board.placePiece("a7", Piece.createBlackPawn());
-//        assertEquals('P', board.getPiece("a7").getRepresentation());
-//        assertEquals(0.5, board.getPawnStrength(), 0.05);
-//
-//        board.placePiece("c7", Piece.createBlackPawn());
-//        assertEquals('P', board.getPiece("a7").getRepresentation());
-//        assertEquals(0.5, board.getPawnStrength(), 0.05);
-//
-//        board.placePiece("d7", Piece.createBlackBishop());
-//        assertEquals('B', board.getPiece("d7").getRepresentation());
-//        assertEquals(2.5, board.getBishopStrength(), 2.05);
-//
-//    }
+    @Test
+    public void testNumberOfPiecesByRepresentation(){
+        board.initialize();
+        assertEquals(2, board.pieceCountTurboVersion('r'));
+        assertEquals(2, board.pieceCountTurboVersion('n'));
+        assertEquals(2 ,board.pieceCountTurboVersion(('b')));
+        assertEquals(1, board.pieceCountTurboVersion(('q')));
+        assertEquals(1, board.pieceCountTurboVersion(('k')));
+
+        assertEquals(8, board.pieceCountTurboVersion(('p')));
+
+        assertEquals(8, board.pieceCountTurboVersion(('P')));
+
+        assertEquals(2, board.pieceCountTurboVersion(('R')));
+        assertEquals(2, board.pieceCountTurboVersion(('N')));
+        assertEquals(2, board.pieceCountTurboVersion(('B')));
+        assertEquals(1, board.pieceCountTurboVersion(('Q')));
+        assertEquals(1, board.pieceCountTurboVersion(('K')));
+        Piece.resetCounter();
+    }
+    @Test
+    public void testRetrievePieceByLocation(){
+        board.initialize();
+        assertEquals('R', board.getPiece("a8").getRepresentation());
+        assertEquals('N', board.getPiece("b8").getRepresentation());
+        assertEquals('B', board.getPiece("c8").getRepresentation());
+        assertEquals('n', board.getPiece("b1").getRepresentation());
+
+    }
+    @Test
+    public void testEmptyBoard(){
+        assertEquals(
+                StringUtil.appendNewLine("........") +
+                        StringUtil.appendNewLine("........") +
+                        StringUtil.appendNewLine("........") +
+                        StringUtil.appendNewLine("........") +
+                        StringUtil.appendNewLine("........") +
+                        StringUtil.appendNewLine("........") +
+                        StringUtil.appendNewLine("........") +
+                        StringUtil.appendNewLine("........"),
+                board.printBoard());
+    }
+    @Test
+    public void testPieceByLocation(){
+        board.initialize();
+        board.placePieceRemastered("b6",Piece.createBlackKing());
+        assertEquals('K', board.getPiece("b6").getRepresentation());
+        board.placePieceRemastered("b7",Piece.createBlackKing());
+        assertEquals('K', board.getPiece("b7").getRepresentation());
+    }
+    @Test
+    public void testPieceStrength(){
+        board.initialize();
+
+        board.placePieceRemastered("c8",Piece.createBlackRook());
+        assertEquals('R', board.getPiece("c8").getRepresentation());
+        assertEquals(5, board.basicStrength(), 0.05);
+
+    }
 }
